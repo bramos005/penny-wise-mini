8c62c2d98ef82cc514542c3ef7ae98910cd37f1a
Brandon Ramos <bramos005@citymail.cuny.edu>
2024-01-20 10:27:16 -0500

    removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,37 +18,6 @@ export default function Home() {
           </button>
         </Link>
       </div>
-
-      {/* <main className=" flex  justify-center flex-wrap   text-white gap-20 items-center mt-[10rem] sm:">
-        <div className="flex flex-col   gap-5">
-          <h2 className="text-[2rem] text-semibold flex it">
-            Plan. Spend. <p className="ml-3 text-custom">Save.</p>
-          </h2>
-         
-          <p className="w-96 text-semibold text-xl  ">
-            Discover a simpler way to budget and save. With Penny Wise,
-            you're not just tracking expenses – you're paving the path to
-            financial freedom. Effortless, intuitive, and smart – your journey
-            towards smarter spending starts here.
-          </p>
-
-          <button className="self-center w-[10rem] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50">
-            Get Started
-        </button>
-        </div>
-        <div>
-        
-        </div>
-
-        <div className="flex border-[10px] border-trans-white rounded-lg">
-          <Image
-            src={"/dash.png"}
-            width={600}
-            height={200}
-            alt="dashboard-img"
-          />
-        </div>
-      </main> */}
     </div>
   );
 }
